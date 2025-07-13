30b758372f2c026973e8ea10b8181615e011a9fd
Andrew Griffiths <mail@andrewgriffithsonline.com>
2025-07-13 11:13:18 +0100

    s

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 cat "$SCRIPT_DIR/.gitconfig" >> "$HOME/.gitconfig"
-ln -sf "$SCRIPT_DIR/.bash_aliases" "$HOME/.bash_aliases"
-ln -sf "$SCRIPT_DIR/.bash_aliases_git" "$HOME/.bash_aliases_git"
-ln -sf "$SCRIPT_DIR/.bash_aliases_tribe" "$HOME/.bash_aliases_tribe"
+
+if [[ -f $HOME/.bash_aliases ]]; then
+	rm -f "$HOME/.bash_aliases"
+	ln -s "$SCRIPT_DIR/.bash_aliases" "$HOME/.bash_aliases"
+	ln -s "$SCRIPT_DIR/.bash_aliases_git" "$HOME/.bash_aliases_git"
+	ln -s "$SCRIPT_DIR/.bash_aliases_tribe" "$HOME/.bash_aliases_tribe"
+fi
+
