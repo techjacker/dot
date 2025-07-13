515e919f13d5c86c8ce8a530afe1caf595d5f899
Andrew Griffiths <mail@andrewgriffithsonline.com>
2025-07-13 11:24:05 +0100

    s

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # cat "$SCRIPT_DIR/.gitconfig" >> "$HOME/.gitconfig"
 
-if [[ -f $HOME/.bash_aliases ]]; then
-	rm -f "$HOME/.bash_aliases"
-	ln -s "$SCRIPT_DIR/.bash_aliases" "$HOME/.bash_aliases"
-	ln -s "$SCRIPT_DIR/.bash_aliases_git" "$HOME/.bash_aliases_git"
-	ln -s "$SCRIPT_DIR/.bash_aliases_tribe" "$HOME/.bash_aliases_tribe"
-fi
 
+# if [[ -f $HOME/.bash_aliases ]]; then
+# 	rm -f "$HOME/.bash_aliases"
+# fi
+ln -sf "$SCRIPT_DIR/.bash_aliases" "$HOME/.bash_aliases"
+ln -sf "$SCRIPT_DIR/.bash_aliases_git" "$HOME/.bash_aliases_git"
+ln -sf "$SCRIPT_DIR/.bash_aliases_tribe" "$HOME/.bash_aliases_tribe"
+ln -sf "$SCRIPT_DIR/.gitconfig" >> "$HOME/.gitconfig.andy"
+
+
+# [include]
+#     path = /home/vscode/.gitconfig.andy
